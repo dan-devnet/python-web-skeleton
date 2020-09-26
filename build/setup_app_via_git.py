5110c3ab51b54c1c05daf1f1229c235dd35a6e33
devd4n <devd4n@local.local>
2020-09-26 13:30:35 +0200

    update automatism to create config files 2

--- build/setup_app_via_git.py
+++ build/setup_app_via_git.py
@@ -1,12 +1,16 @@
 import subprocess
 import os
-from build_vars import DIR_BASE, DIR_APP
+from config import DIR_BASE, DIR_APP
 
-choice = input("!!! WARNING !!! This will delete all files in the app directory! Continue? [y] or [n]")
+def setup_app_via_git():
+    choice = input("!!! WARNING !!! This will delete all files in the app directory! Continue? [y] or [n]")
 
-if (choice == 'y'):
-    os.system("rm -rf " + DIR_APP + "/*")
-    git_repo = input("Git-repo-URL:")
-    command = "git init && git clone " + git_repo + " " + str(DIR_APP)
-    git_pull_debug = subprocess.getoutput(command)
-    print(git_pull_debug)
+    if (choice == 'y'):
+        os.system("rm -rf " + DIR_APP + "/*")
+        git_repo = input("Git-repo-URL:")
+        command = "git init && git clone " + git_repo + " " + str(DIR_APP)
+        git_pull_debug = subprocess.getoutput(command)
+        print(git_pull_debug)
+
+if __name__ == '__main__':
+    setup_app_via_git()
